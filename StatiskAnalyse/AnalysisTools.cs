3faff436602422fef155c416b74d1fd1580f4996
PerHN123 <PerNielsen123@gmail.com>
2017-06-16 10:40:33 +0200

    gn

--- StatiskAnalyse/AnalysisTools.cs
+++ StatiskAnalyse/AnalysisTools.cs
@@ -1,13 +1,33 @@
-﻿namespace StatiskAnalyse
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+using StatiskAnalyse.ResultWrappers;
+
+namespace StatiskAnalyse
 {
     public static class AnalysisTools
     {
 
-
-
         public static void TraceStringBuilder(string a)
         {
             
+        }
+
+        public static void TraceMethodCall(ApkAnalysis a, Use result, int i)
+        {
+            string function = result.FoundIn.Source[i].Split( ).Last();
+            Tuple<int, string>[] calledlocations;
+            string pattern = @"invoke-\w*";
+            Console.WriteLine(a.Root.DirPath);
+            var uses = a.Root.FindUses(new Regex(pattern));
+
+
+
+
+            return;
         }
     }
 }
